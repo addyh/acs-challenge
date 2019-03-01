1410148dc0c3fcbca2214ba1c2f03671b48f5744
addyh <addisonhatcher@gmail.com>
2019-03-01 11:13:16 -0500

    create vue components

---
+++ resources/js/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <h1 class="main-title">ACS Test Panel Results</h1>
+
+
+          <div id="panel-list"><Panel></Panel></div>
+
+    </div>
+</template>
+
+<script>
+   import Panel from './Panel.vue';
+
+    export default {
+        name: "App",
+        components: {
+          Panel
+        },
+        mounted() {
+            console.log('App mounted.');
+        }
+    }
+</script>
---
+++ resources/js/components/Panel.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header panels">Panel Name</div>
+
+                    <div class="card-body">Panel Content</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import LabTest from './LabTest.vue';
+
+    export default {
+      name: "Panel",
+      components: {
+          LabTest
+      },
+        mounted() {
+            console.log('Panel mounted.');
+        }
+    }
+</script>
