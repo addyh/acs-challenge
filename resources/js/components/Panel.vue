c78c588f8c70f738ca7c8ac0eb43568d5d50b57a
addyh <13260884+addyh@users.noreply.github.com>
2019-03-01 13:21:29 -0500

    panel results

--- resources/js/components/Panel.vue
+++ resources/js/components/Panel.vue
@@ -3,14 +3,34 @@
         <div class="row justify-content-center">
             <div class="col-md-12">
                 <div v-bind:key="panel.id" v-for="panel in panels" class="card">
+
                     <div class="card-header">
-                        {{panel.name}} &mdash;
-                        <span class="abbreviation">({{panel.abbreviation}})</span>
-                    </div>
+                    <table class="panel-table">
+                        <tr>
+
+                            <td class="panel-header">
+                                    {{panel.name}}
+                            </td>
+
+                            <td>
+                                <div class="panel-description">
+                                Panel ID: &nbsp;&nbsp;&nbsp;&nbsp;{{panel.id}}<br />
+                                Abbreviation: {{panel.abbreviation}}<br />
+                                Alias: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{panel.alias}}<br />
+                                Description: &nbsp;{{panel.description}}<br />
+                                Identifier: &nbsp;&nbsp;{{panel.identifier}}<br />
+                                Created at: &nbsp;&nbsp;{{panel.created_at}}<br />
+                                Updated at: &nbsp;&nbsp;{{panel.updated_at}}
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
 
                     <div class="card-body">
                         <LabTest v-bind:panelId="panel.id" v-bind:labTests="labTests" />
                     </div>
+
                 </div>
             </div>
         </div>
@@ -42,12 +62,23 @@
         margin-top: 40px;
     }
 
-    .card-header {
+    .abbreviation {
+        font-size: 16pt;
+    }
+
+    .panel-table {
+        width: 100%;
+        border-width: 0px;
+    }
+
+    .panel-header {
         font-weight: bold;
         font-size: 25pt;
     }
 
-    .abbreviation {
-        font-size: 16pt;
+    .panel-description {
+        font-family: Courier;
+        line-height: 85%;
     }
+
 </style>
