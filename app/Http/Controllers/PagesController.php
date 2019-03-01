696c7db5e977021ff6a5859ffdf8a29b2ca65dbb
addyh <addisonhatcher@gmail.com>
2019-03-01 06:39:35 -0500

    update index

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,21 +7,9 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index() {
-        $title = "welcome to laravel..";
+        $title = "welcome";
         //return view('pages.index', compact('title'));
         return view('pages.index')->with('title', $title);
     }
 
-    public function about() {
-      $title = "the about us";
-        return view('pages.about')->with('title', $title);
-    }
-
-    public function services() {
-      $data = array(
-        'title' => 'Services',
-        'services' => ['Web Design', 'Programming','seo']
-      );
-        return view('pages.services')->with($data);
-    }
 }
