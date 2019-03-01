6a78f2eb2e4f08ca8cb1cb9bf67af0903af1702d
addyh <13260884+addyh@users.noreply.github.com>
2019-03-01 06:39:35 -0500

    update index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,1 +1,16 @@
-indx
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>ACS Test Panels</title>
+    </head>
+    <body>
+      <div id="app">
+
+app
+
+      </div>
+    </body>
+</html>
