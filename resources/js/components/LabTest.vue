dd1c2a99dd6ada3cef18f9d5ff16fedbd3df6e14
addyh <13260884+addyh@users.noreply.github.com>
2019-03-01 13:00:19 -0500

    show test results

--- resources/js/components/LabTest.vue
+++ resources/js/components/LabTest.vue
@@ -2,10 +2,8 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header panels">Panel Name</div>
-
-                    <div class="card-body">Panel Content</div>
+                <div v-if="panelId == labTest.panel_id" v-bind:key="labTest.id" v-for="labTest in labTests" class="card">
+                    <div class="card-body">{{labTest.abbreviation}} id: {{labTest.panel_id}}</div>
                 </div>
             </div>
         </div>
@@ -13,11 +11,12 @@
 </template>
 
 <script>
+    import Panel from './Panel.vue';
 
     export default {
       name: "LabTest",
+      props: ["labTests", "panelId"],
       components: {
-
       },
         mounted() {
             console.log('LabTest mounted.');
